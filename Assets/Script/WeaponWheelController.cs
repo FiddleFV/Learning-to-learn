96d64fb02c141b5fd547937f2b1e0788514db228
FiddleVC <kevinbohlin.private@gmail.com>
2023-09-27 15:54:25 +0200

    Weapon selection menu

---
+++ Assets/Script/WeaponWheelController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponWheelController : MonoBehaviour
+{
+    public Animator anim;
+    private bool weaponWheelSelected = false;
+    public Image selectedItem;
+    public Sprite noImage;
+    public static int weaponID;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            weaponWheelSelected = !weaponWheelSelected;
+        }
+
+        if (weaponWheelSelected)
+        {
+            anim.SetBool("OpenWeaponWheel", true);
+        }
+        else
+        {
+            anim.SetBool("OpenWeaponWheel", false);
+        }
+
+        switch (weaponID)
+        {
+            case 0: //nothing is selected
+                selectedItem.sprite = noImage;
+                break;
+            case 1: //Axe
+                Debug.Log("Axe");
+                break;
+            case 2: //Sword
+                Debug.Log("Sword");
+                break;
+            case 3: //Dagger
+                Debug.Log("Dagger");
+                break;
+        }
+
+    }
+}
